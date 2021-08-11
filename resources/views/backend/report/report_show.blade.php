e87988c400ad74b3fc96fe212a314d68656796cb
hxdayah98 <hxdayah98@gmail.com>
2021-08-12 07:03:51 +0800

    changes

--- resources/views/backend/report/report_show.blade.php
+++ resources/views/backend/report/report_show.blade.php
@@ -3,22 +3,22 @@
 
 
   <!-- Content Wrapper. Contains page content -->
-  
+
 	  <div class="container-full">
 		<!-- Content Header (Page header) -->
-		 
+
 
 		<!-- Main content -->
 		<section class="content">
 		  <div class="row">
-			   
-		 
+
+
 
 			<div class="col-12">
 
 			 <div class="box">
 				<div class="box-header with-border">
-				  <h3 class="box-title">Order List</h3>
+				  <h3 class="box-title">Transaction per Period</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
@@ -32,7 +32,7 @@
 								<th>Payment </th>
 								<th>Status </th>
 								<th>Action</th>
-								 
+
 							</tr>
 						</thead>
 						<tbody>
@@ -43,7 +43,7 @@
 		<td> ${{ $item->amount }}  </td>
 
 		<td> {{ $item->payment_method }}  </td>
-		
+
 		<td> <span class="badge badge-pill badge-primary">{{ $item->status }} </span>  </td>
 
 		<td width="25%">
@@ -52,11 +52,11 @@
  <a target="_blank" href="{{ route('invoice.download',$item->id) }}" class="btn btn-danger" title="Invoice Download">
  	<i class="fa fa-download"></i></a>
 		</td>
-							 
+
 	 </tr>
 	  @endforeach
 						</tbody>
-						 
+
 					  </table>
 					</div>
 				</div>
@@ -64,22 +64,22 @@
 			  </div>
 			  <!-- /.box -->
 
-			          
+
 			</div>
 			<!-- /.col -->
 
- 
 
- 
+
+
 
 
 		  </div>
 		  <!-- /.row -->
 		</section>
 		<!-- /.content -->
-	  
+
 	  </div>
-  
+
 
 
 
