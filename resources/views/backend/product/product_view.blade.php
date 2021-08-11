4173b6e7f86ed06dc33bb43d8a67955caebbc8f9
hxdayah98 <hxdayah98@gmail.com>
2021-08-12 05:28:12 +0800

    changes

--- resources/views/backend/product/product_view.blade.php
+++ resources/views/backend/product/product_view.blade.php
@@ -72,8 +72,6 @@
 
 
 		<td width="30%">
- <a href="{{ route('product.edit',$item->id) }}" class="btn btn-primary" title="Product Details Data"><i class="fa fa-eye"></i> </a>
-
  <a href="{{ route('product.edit',$item->id) }}" class="btn btn-info" title="Edit Data"><i class="fa fa-pencil"></i> </a>
 
  <a href="{{ route('product.delete',$item->id) }}" class="btn btn-danger" title="Delete Data" id="delete">
